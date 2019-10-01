9da33ad3b6c2fdcee2bdacb42442007f6b790da6
venelrene <venelrene@gmail.com>
2019-10-01 13:22:27 -0400

    added the complere button feature

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -23,4 +23,22 @@ RSpec.feature "Projects", type: :feature do
       end
     }.to change(user.projects, :count).by(1)
   end
+
+	scenario "user completes a project", focus: true do
+		user = FactoryBot.create(:user)
+		project = FactoryBot.create(:project, owner: user)
+		login_as user, scope: :user
+
+		visit project_path(project)
+
+		expect(page).to_not have_content "Completed"
+
+		click_button "Complete"
+
+		expect(project.reload.completed?).to be true
+		expect(page).to \
+			have_content "Congratulations, this project is complete!"
+		expect(page).to have_content "Completed"
+		expect(page).to_not have_button "Complete"
+	end
 end
